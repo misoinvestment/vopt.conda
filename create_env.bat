d05fc1a3eb1433dcf61ac7a247e1ff7ac77d5cfe
Joel Kim <kim.dohhyoung@gmail.com>
2017-03-29 20:51:21 +0900

    윈도우즈에서 NumPy+MKL 문제로 cvxopt 사용이 불가능한 문제 해결

--- create_env.bat
+++ create_env.bat
@@ -3,12 +3,14 @@ cd /D %~dp0
 conda create --name vopt --yes pip menuinst
 call activate vopt
 
-REM for Windows only
+REM Windows only. Numpy and related packages must be installed using pip
 pip install windows_packages\numpy-1.12.0+mkl-cp27-cp27m-win_amd64.whl
 pip install windows_packages\scipy-0.19.0-cp27-cp27m-win_amd64.whl
 pip install windows_packages\cvxopt-1.1.9-cp27-cp27m-win_amd64.whl
 pip install windows_packages\cvxpy-0.4.9-py2-none-any.whl
+pip install matplotlib pandas seaborn statsmodels
 
-conda install --yes --quiet django ipython matplotlib nose notebook pandas psycopg2 qtconsole scipy seaborn statsmodels xlrd xlwt
+conda install --yes --quiet django ipython nose notebook psycopg2 qtconsole xlrd xlwt
+
 call deactivate
 cmd.exe /K
--- create_env.sh
+++ create_env.sh
@@ -3,6 +3,7 @@
 conda create --name vopt --yes pip python && \
 source activate vopt && \
 conda install --yes --quiet -c conda-forge cvxopt && \
-conda install --yes --quiet -c cvxgrp cvxpy && 
-conda install --yes --quiet django ipython matplotlib nose notebook pandas psycopg2 qtconsole scipy seaborn statsmodels xlrd xlwt && \
+conda install --yes --quiet -c cvxgrp cvxpy && \
+conda install --yes --quiet matplotlib pandas seaborn statsmodels && \
+conda install --yes --quiet django ipython nose notebook psycopg2 qtconsole xlrd xlwt && \
 source deactivate
