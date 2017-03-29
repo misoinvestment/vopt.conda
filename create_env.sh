e0311cda6284b84e20ad6fb10634485eda416196
Joel Kim <kim.dohhyoung@gmail.com>
2017-03-29 21:25:05 +0900

    패키지 추가: coverage, dateutil

--- create_env.sh
+++ create_env.sh
@@ -5,5 +5,5 @@ source activate vopt && \
 conda install --yes --quiet -c conda-forge cvxopt && \
 conda install --yes --quiet -c cvxgrp cvxpy && \
 conda install --yes --quiet matplotlib pandas seaborn statsmodels && \
-conda install --yes --quiet django ipython nose notebook psycopg2 qtconsole xlrd xlwt && \
+conda install --yes --quiet coverage dateutil django ipython nose notebook psycopg2 qtconsole xlrd xlwt && \
 source deactivate
