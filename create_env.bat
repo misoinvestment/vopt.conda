2044962191bd765ac15400b88e66ccd395c13b0c
Joel Kim <kim.dohhyoung@gmail.com>
2017-03-29 20:14:40 +0900

    윈도우즈에서 메뉴생성이 안되는 버그 수정

--- create_env.bat
+++ create_env.bat
@@ -1,12 +1,14 @@
 cd /D %~dp0
 
-conda create --name vopt --yes
+conda create --name vopt --yes pip menuinst
 call activate vopt
 
+REM for Windows only
 pip install windows_packages\numpy-1.12.0+mkl-cp27-cp27m-win_amd64.whl
+pip install windows_packages\scipy-0.19.0-cp27-cp27m-win_amd64.whl
 pip install windows_packages\cvxopt-1.1.9-cp27-cp27m-win_amd64.whl
 pip install windows_packages\cvxpy-0.4.9-py2-none-any.whl
 
-conda install --yes --quiet django ipython matplotlib nose notebook pandas psycopg2 qtconsole scipy statsmodels xlrd xlwt
+conda install --yes --quiet django ipython matplotlib nose notebook pandas psycopg2 qtconsole scipy seaborn statsmodels xlrd xlwt
 call deactivate
 cmd.exe /K
