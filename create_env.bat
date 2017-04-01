2b96d0eba0ac6d01cf97cc46defc1c7d3e048192
Joel Kim <kim.dohhyoung@gmail.com>
2017-04-01 21:12:55 +0900

    이슈 #1 해결을 위해 윈도우즈용 패키지 바이너리 추가

--- create_env.bat
+++ create_env.bat
@@ -7,6 +7,7 @@ REM Windows only. Numpy and related packages must be installed using pip
 pip install windows_packages\numpy-1.12.0+mkl-cp27-cp27m-win_amd64.whl
 pip install windows_packages\scipy-0.19.0-cp27-cp27m-win_amd64.whl
 pip install windows_packages\cvxopt-1.1.9-cp27-cp27m-win_amd64.whl
+pip install windows_packages\CVXcanon-0.1.1-cp27-cp27m-win_amd64.whl
 pip install windows_packages\cvxpy-0.4.9-py2-none-any.whl
 pip install matplotlib pandas seaborn statsmodels
 
