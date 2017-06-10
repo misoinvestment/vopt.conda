e3a14c94c623b6cf774209b5ad0726c516e8fbce
joelkim <kim.dohhyoung@gmail.com>
2017-06-10 21:09:00 +0900

    remove duplicated packages from install script

--- create_env.sh
+++ create_env.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-conda create --name vopt --yes python=3 pip && \
+conda create --name vopt --yes python=3.5 pip && \
 source activate vopt && \
 conda install --yes --quiet -c cvxgrp cvxopt cvxpy && \
 pip install coreapi django django-crispy-forms django-filter django-guardian django-jinja djangorestframework json-rpc && \
-conda install --yes --quiet coverage markdown psycopg2 && \
+conda install --yes --quiet anaconda coverage markdown psycopg2 && \
 source deactivate
