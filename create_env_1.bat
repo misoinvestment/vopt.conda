ac1bb42d93636870d8c5bd77e2b157f38c40bd72
joelkim <kim.dohhyoung@gmail.com>
2017-06-15 11:50:40 +0900

    설치 패키지 명시

--- create_env_1.bat
+++ create_env_1.bat
@@ -3,7 +3,7 @@ cd /D %~dp0
 conda create --name vopt --yes python=3.5 pip menuinst
 call activate vopt
 
-conda install --yes --quiet anaconda coverage markdown psycopg2
+conda install --yes --quiet anaconda coverage django markdown psycopg2
 
 call deactivate
 cmd.exe /K
