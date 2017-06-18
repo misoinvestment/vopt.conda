77b0d0d9c3c750954431e93eb27a4d23758accab
joelkim <kim.dohhyoung@gmail.com>
2017-06-18 10:59:07 +0900

    python eve 패키지 추가

--- create_env.sh
+++ create_env.sh
@@ -4,5 +4,8 @@ conda create --name vopt --yes python=3.5 pip && \
 source activate vopt && \
 conda install --yes --quiet anaconda coverage django markdown psycopg2 && \
 conda install --yes --quiet -c cvxgrp cvxopt cvxpy && \
-pip install coreapi django-crispy-forms django-filter django-guardian django-jinja djangorestframework json-rpc && \
+pip install \
+coreapi django-crispy-forms django-filter django-guardian django-jinja djangorestframework && \
+eve json-rpc && \
+echo
 source deactivate
