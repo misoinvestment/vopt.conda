2a960482620c0cf85ac90d25c082673db31e8604
JoelKim <kim.dohhyoung@gmail.com>
2017-06-18 15:27:54 +0900

    중국 주식 정보를 얻기 위한 tushare 패키지 추가

--- create_env.sh
+++ create_env.sh
@@ -5,7 +5,7 @@ source activate vopt && \
 conda install --yes --quiet anaconda coverage django markdown psycopg2 && \
 conda install --yes --quiet -c cvxgrp cvxopt cvxpy && \
 pip install \
-coreapi django-crispy-forms django-filter django-guardian django-jinja djangorestframework && \
-eve json-rpc && \
-echo
+coreapi django-crispy-forms django-filter django-guardian django-jinja djangorestframework \
+eve json-rpc tushare \
+&& echo
 source deactivate
--- create_env_3.bat
+++ create_env_3.bat
@@ -13,7 +13,9 @@ pip install windows_packages\multiprocess-0.70.5-cp35-cp35m-win_amd64.whl
 pip install windows_packages\ecos-2.0.4-cp35-cp35m-win_amd64.whl
 pip install windows_packages\cvxpy-0.4.10-py3-none-any.whl
 
-pip install coreapi django-crispy-forms django-filter django-guardian django-jinja djangorestframework json-rpc
+pip install ^
+coreapi django-crispy-forms django-filter django-guardian django-jinja djangorestframework ^
+eve json-rpc tushare
 
 call deactivate
 cmd.exe /K
