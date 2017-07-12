63d17424959838bbdac32c0eccbc6a1c81b25d6d
joelkim <kim.dohhyoung@gmail.com>
2017-07-12 19:21:03 +0900

    change cvxopt install from conda to pip due to GLPK

--- create_env_mac_2.sh
+++ create_env_mac_2.sh
@@ -2,8 +2,8 @@
 
 source activate vopt && \
 conda install --yes --quiet anaconda coverage django=1.10 flask markdown psycopg2 && \
-conda install --yes --quiet -c cvxgrp cvxopt cvxpy && \
 pip install \
+cvxopt cvxpy \
 coreapi django-crispy-forms django-filter django-guardian django-jinja djangorestframework \
 eve flask-restplus flask-security flask_sqlalchemy json-rpc SQLAlchemy-Continuum tushare \
 && echo
