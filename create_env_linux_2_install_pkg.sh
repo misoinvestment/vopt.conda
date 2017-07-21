7a5b946a45baed2c1c4934014f09fc2d98baaf98
joelkim <kim.dohhyoung@gmail.com>
2017-07-21 19:24:09 +0900

    add CyLP to linux

--- create_env_linux_2_install_pkg.sh
+++ create_env_linux_2_install_pkg.sh
@@ -2,9 +2,21 @@
 
 source activate vopt && \
 conda install --yes --quiet anaconda coverage django=1.10 flask markdown psycopg2 && \
+conda update --yes --quiet libgcc && \
 pip install \
 cvxopt cvxpy \
 coreapi django-crispy-forms django-filter django-guardian django-jinja djangorestframework \
 eve flask-restplus flask-security flask_sqlalchemy json-rpc SQLAlchemy-Continuum tushare \
 && echo
+
+cd ~ && \
+mkdir -p Code && \
+cd Code && \
+rm -rf CyLP && \
+git clone https://github.com/jjhelmus/CyLP.git &&
+cd CyLP/ && \
+git checkout py3 && \
+python setup.py install && \
+echo
+
 source deactivate
--- create_env_mac_3_install_pkg.sh
+++ create_env_mac_3_install_pkg.sh
@@ -2,6 +2,7 @@
 
 source activate vopt && \
 conda install --yes --quiet anaconda coverage django=1.10 flask markdown psycopg2 && \
+conda update --yes --quiet libgcc && \
 pip install \
 cvxopt cvxpy \
 coreapi django-crispy-forms django-filter django-guardian django-jinja djangorestframework \
