851493eba3a1af4519e6011dea3c950ebba5fb19
joelkim <kim.dohhyoung@gmail.com>
2017-08-08 18:44:03 +0900

    add awscli for deployment

--- install_pkg.bat
+++ install_pkg.bat
@@ -14,7 +14,7 @@ pip install windows_packages\ecos-2.0.4-cp35-cp35m-win_amd64.whl
 pip install windows_packages\cvxpy-0.4.10-py3-none-any.whl
 
 pip install ^
-coreapi django-crispy-forms django-filter django-guardian django-jinja djangorestframework ^
+awscli coreapi django-crispy-forms django-filter django-guardian django-jinja djangorestframework ^
 eve flask-restplus flask-security flask_sqlalchemy json-rpc SQLAlchemy-Continuum tushare
 
 conda install --yes --quiet -c conda-forge fabric3
