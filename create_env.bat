29a52e1b7d20d10fcd43c2c25821a0c0bea4462b
joelkim <kim.dohhyoung@gmail.com>
2017-08-09 12:21:39 +0900

    add gunicorn, gevent, greenlet

--- create_env.bat
+++ create_env.bat
@@ -3,8 +3,7 @@ cd /D %~dp0
 conda create --name vopt --yes python=3.5 pip menuinst
 call activate vopt
 
-conda install --yes --quiet anaconda coverage django=1.10 flask markdown psycopg2
-conda update --yes --quiet libgcc
+conda install --yes --quiet anaconda coverage django=1.10 flask gevent greenlet gunicorn markdown psycopg2
 
 call deactivate
 cmd.exe /K
--- install_pkg.sh
+++ install_pkg.sh
@@ -1,15 +1,30 @@
 #!/usr/bin/env bash
 
+case "$(uname -s)" in
+
+   Darwin)
+     echo "Settings for OS X..."
+     export COIN_INSTALL_DIR=/usr/local/src/Cbc-2.8.9
+     export LD_LIBRARY_PATH="/usr/local/src/Cbc-2.8.9/lib:$LD_LIBRARY_PATH"
+     export GLPK_LIB_DIR=/usr/local/lib
+     export GLPK_INC_DIR=/usr/local/include
+     export BUILD_GLPK=1
+     ;;
+
+esac
+
+echo "Python package installing..."
 source activate vopt && \
-conda install --yes --quiet anaconda coverage django=1.10 flask markdown psycopg2 && \
-conda install --yes --quiet -c conda-forge fabric3 && \
+conda install --yes --quiet anaconda coverage django=1.10 flask gevent greenlet gunicorn markdown psycopg2 && \
 conda update --yes --quiet libgcc && \
+conda install --yes --quiet -c conda-forge fabric3 && \
 pip install \
 cvxopt cvxpy \
 awscli coreapi django-crispy-forms django-filter django-guardian django-jinja djangorestframework \
-eve flask-restplus flask-security flask_sqlalchemy json-rpc SQLAlchemy-Continuum tushare \
+eve flask-restplus flask-security flask_sqlalchemy json-rpc SQLAlchemy-Continuum tushare uwsgi \
 && echo
 
+echo "CyLP package installing..."
 cd ~ && \
 mkdir -p Code && \
 cd Code && \
@@ -18,6 +33,7 @@ git clone https://github.com/jjhelmus/CyLP.git &&
 cd CyLP/ && \
 git checkout py3 && \
 python setup.py install && \
+cd ~ && \
 echo
 
 source deactivate
