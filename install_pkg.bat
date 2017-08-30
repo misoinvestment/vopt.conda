1573d26f1f64d4752834c22f9cdc593df1ebc8b2
joelkim <kim.dohhyoung@gmail.com>
2017-08-30 16:16:18 +0900

    re-arrange install order to avoid conflict

--- install_pkg.bat
+++ install_pkg.bat
@@ -5,7 +5,8 @@ set BUILD_GLPK=1
 call activate vopt
 
 conda install --yes --quiet ^
-anaconda coverage cvxopt django=1.10 flask gevent greenlet markdown psycopg2 sphinx_rtd_theme && ^
+anaconda coverage ecos django=1.10 flask gevent greenlet markdown psycopg2 sphinx_rtd_theme && ^
+conda install --yes --quiet cvxopt && ^
 conda install --yes --quiet -c conda-forge fabric3 && ^
 pip install ^
 awscli coreapi cvxpy django-crispy-forms django-filter django-guardian django-jinja djangorestframework ^
