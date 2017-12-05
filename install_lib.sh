3990c2045bce8d6863b4334083479b24a919e45e
joelkim <kim.dohhyoung@gmail.com>
2017-12-05 16:15:52 +0900

    fix install_lib.sh

--- install_lib.sh
+++ install_lib.sh
@@ -3,30 +3,30 @@
 case `uname` in
 
     Linux)
-	if type lsb_release >/dev/null 2>&1; then
-	    OS=$(lsb_release -si)
-	    VER=$(lsb_release -sr)
-	    if [ "$OS" == "Ubuntu" ] && [ "$VER" == "16.04" ]; then
-		bash install_lib_ubuntu.sh
-	    else
-		echo "Error! Support Ubuntu only."
-		return
-	    fi
-	else
-	    echo "Error! Cannot find linux version."
-	    return
-	fi
-	;;
+        if type lsb_release >/dev/null 2>&1; then
+            OS=$(lsb_release -si)
+            VER=$(lsb_release -sr)
+            if [ "$OS" == "Ubuntu" ] && [ "$VER" == "16.04" ]; then
+                /bin/bash install_lib_ubuntu.sh
+            else
+                echo "Error! Support Ubuntu only."
+                return
+            fi
+        else
+            echo "Error! Cannot find linux version."
+            return
+        fi
+        ;;
 
     Darwin)
-	echo "Mac OS X."
-	export MACOSX_DEPLOYMENT_TARGET=10.10
-	;;
+        echo "Mac OS X."
+        export MACOSX_DEPLOYMENT_TARGET=10.10
+        ;;
 
     *)
-	echo "Error! Unsupported OS."
-	return
-	;;
+        echo "Error! Unsupported OS."
+        return
+        ;;
 
 esac
 
