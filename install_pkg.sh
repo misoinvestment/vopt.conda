70adb9d21bef43e2068f9823792b1e8db3305399
s132048 <musicsharing@naver.com>
2017-12-14 18:04:32 +0900

    install_pkg update

--- install_pkg.sh
+++ install_pkg.sh
@@ -10,17 +10,56 @@ export BUILD_GLPK=1
 echo "Python package installing..."
 source activate vopt && \
 conda install --yes --quiet \
-anaconda alembic constantly coverage ecos django=1.10 flask gevent greenlet hyperlink incremental ipyparallel krb5 \
-markdown psycopg2 scrapy sphinx_rtd_theme twisted && \
-conda install --yes --quiet -c conda-forge fabric3 jupyter_nbextensions_configurator && \
-conda update --yes --quiet libgcc && \
+    anaconda \
+    alembic \
+    constantly \
+    coverage \
+    ecos \
+    django=1.10 \
+    flask \
+    gevent \
+    greenlet \
+    hyperlink \
+    incremental \
+    ipyparallel \
+    krb5 \
+    markdown \
+    psycopg2 \
+    scrapy \
+    sphinx_rtd_theme \
+    twisted \
+    && \
+conda install --yes --quiet -c conda-forge \
+    fabric3 \
+    jupyter_nbextensions_configurator \
+    && \
+conda update --yes --quiet \
+    libgcc \
+    && \
 pip install \
-gunicorn pudb uwsgi \
-&& \
+    gunicorn \
+    pudb \
+    uwsgi \
+    && \
 pip install \
-autopep8 awscli coreapi cvxopt cvxpy django-crispy-forms django-filter django-guardian django-jinja djangorestframework \
-eve flask-restplus flask-security flask_sqlalchemy json-rpc SQLAlchemy-Continuum tushare \
-&& \
+    autopep8 \
+    awscli \
+    coreapi \
+    cvxopt \
+    cvxpy \
+    django-crispy-forms \
+    django-filter \
+    django-guardian \
+    django-jinja \
+    djangorestframework \
+    eve \
+    flask-restplus \
+    flask-security \
+    flask_sqlalchemy \
+    json-rpc \
+    SQLAlchemy-Continuum \
+    tushare \
+    && \
 ipcluster nbextension enable && \
 jupyter nbextensions_configurator enable --user && \
 echo
