909633cbfe36afc19be95c06bee903f608af99ab
joelkim <kim.dohhyoung@gmail.com>
2017-12-18 22:01:58 +0900

    python 3.5 -> 3.6, add anaconda coincbc, glpk

--- install_pkg.bat
+++ install_pkg.bat
@@ -1,19 +1,24 @@
 cd /D %~dp0
 
+# set environment
+set CYLP_USE_CYTHON=TRUE
+set COIN_INSTALL_DIR=~/anaconda3/envs/vopt/
+set LD_LIBRARY_PATH="~/anaconda3/envs/vopt/lib:$LD_LIBRARY_PATH"
+set GLPK_LIB_DIR=~/anaconda3/envs/vopt/
+set GLPK_INC_DIR=~/anaconda3/envs/vopt/include
 set BUILD_GLPK=1
 
 call activate vopt
-
 conda install --yes --quiet ^
-anaconda alembic constantly coverage ecos django=1.10 flask gevent greenlet hyperlink incremental ipyparallel krb5 ^
+anaconda alembic constantly coverage ecos django flask gevent greenlet hyperlink incremental ipyparallel krb5 ^
 markdown psycopg2 scrapy sphinx_rtd_theme twisted && ^
 conda install --yes --quiet cvxopt && ^
 conda install --yes --quiet -c conda-forge fabric3 jupyter_nbextensions_configurator && ^
 pip install ^
 autopep8 awscli coreapi cvxpy django-crispy-forms django-filter django-guardian django-jinja djangorestframework ^
-eve flask-restplus flask-security flask_sqlalchemy json-rpc SQLAlchemy-Continuum tushare && ^
-pip install py3_ortools-6.4.4495-cp35-cp35m-win_amd64.whl && ^
+eve flask-restplus flask-security flask_sqlalchemy json-rpc nbsphinx SQLAlchemy-Continuum tushare ^
 ipcluster nbextension enable && ^
 jupyter nbextensions_configurator enable --user && ^
+pip install git+https://github.com/VeranosTech/CyLP.git@py3 ^
 call deactivate
 cmd.exe /K
--- install_pkg.sh
+++ install_pkg.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
 
 # set environment
+export MACOSX_DEPLOYMENT_TARGET=10.10
+export CYLP_USE_CYTHON=TRUE
 export COIN_INSTALL_DIR=~/anaconda3/envs/vopt/
 export LD_LIBRARY_PATH="~/anaconda3/envs/vopt/lib:$LD_LIBRARY_PATH"
-export MACOSX_DEPLOYMENT_TARGET=10.10
+export GLPK_LIB_DIR=~/anaconda3/envs/vopt/
+export GLPK_INC_DIR=~/anaconda3/envs/vopt/include
+export BUILD_GLPK=1
 
 echo "Python package installing..."
 source activate vopt && \
 conda install --yes --quiet \
-anaconda alembic constantly coverage ecos django=1.10 flask gevent greenlet hyperlink incremental ipyparallel krb5 \
+anaconda alembic constantly coverage ecos django flask gevent greenlet hyperlink incremental ipyparallel krb5 \
 markdown psycopg2 scrapy sphinx_rtd_theme twisted && \
-conda install --yes --quiet -c conda-forge coincbc fabric3 fastparquet feather-format jupyter_nbextensions_configurator pyarrow && \
+conda install --yes --quiet -c conda-forge coincbc fabric3 fastparquet feather-format glpk jupyter_nbextensions_configurator pyarrow && \
 conda update --yes --quiet libgcc && \
 pip install \
 gunicorn pudb uwsgi \
@@ -25,38 +29,5 @@ echo
 
 echo "CyLP package for Python3 installing..."
 pip install git+https://github.com/VeranosTech/CyLP.git@py3
-
-case `uname` in
-
-  Linux)
-    if type lsb_release >/dev/null 2>&1; then
-      OS=$(lsb_release -si)
-      VER=$(lsb_release -sr)
-
-      if [ "$OS" == "Ubuntu" ] && [ "$VER" == "16.04" ]; then
-	  pip install py3_ortools-6.4.4495-cp35-cp35m-manylinux1_x86_64.whl
-	  return
-      else
-          echo "Error! Support Ubuntu 16.04 only."
-          return
-      fi
-
-    else
-       echo "Error! Cannot find linux version."
-       return
-    fi
-    ;;
-
-  Darwin)
-     pip install py3_ortools-6.4.4495-cp35-cp35m-macosx_10_6_intel.whl
-     return
-     ;;
-
-  *)
-     echo "Error! Unsupported OS."
-     return
-     ;;
-
-esac
 
 source deactivate
