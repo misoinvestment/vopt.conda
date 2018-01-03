eebabeeabcd00eef36bf816c56eb4be5b16bdeb8
joelkim <kim.dohhyoung@gmail.com>
2018-01-03 17:02:19 +0900

    update install packages scripts

--- install_pkg.bat
+++ install_pkg.bat
@@ -1,20 +1,36 @@
 cd /D %~dp0
 
-# set environment
+# environment variables
 set GLPK_LIB_DIR=C:\Anaconda3\envs\vopt\Library\lib
 set GLPK_INC_DIR=C:\Anaconda3\envs\vopt\Library\include
 set BUILD_GLPK=1
 
+echo "Python package installing..."
 call activate vopt
+
+echo "Common Anaconda Packages:" && ^
 conda install --yes --quiet ^
-anaconda alembic beautifulsoup4 constantly coverage cvxcanon cvxopt ecos django flask gevent greenlet hyperlink incremental ipyparallel krb5 \
-markdown pymongo psycopg2 requests scrapy simplejson sphinx sphinx_rtd_theme toolz twisted werkzeug && ^
-conda install --yes --quiet -c conda-forge fabric3 fastparquet feather-format glpk jupyter_nbextensions_configurator multiprocess pyarrow && ^
+anaconda alembic beautifulsoup4 constantly coverage cvxcanon cvxopt django ecos flask ^
+gevent greenlet hyperlink incremental ipyparallel krb5 ^
+markdown pymongo psycopg2 requests scrapy simplejson sphinx sphinx_rtd_theme ^
+toolz twisted werkzeug ^
+&& ^
+echo "Common Anaconda Packages in Conda-Forge:" && ^
+conda install --yes --quiet -c conda-forge ^
+awscli fabric3 fastparquet feather-format glpk jupyter_nbextensions_configurator ^
+multiprocess pyarrow ^
+&& ^
+echo "Common pip Packages:" && ^
 pip install ^
-autopep8 awscli coreapi cvxpy django-crispy-forms django-filter django-guardian django-jinja djangorestframework ^
-eve flask-restplus flask-security flask_sqlalchemy json-rpc nbsphinx SQLAlchemy-Continuum tushare ^
+autopep8 awscli coreapi cvxpy django-crispy-forms django-filter django-guardian ^
+django-jinja djangorestframework ^
+eve flask-restplus flask-security flask_sqlalchemy json-rpc nbsphinx ^
+SQLAlchemy-Continuum tushare ^
+&& ^
+echo "Jupyter notebook setting:" && ^
 ipcluster nbextension enable --user && ^
 jupyter nbextensions_configurator enable --user && ^
-pip install git+https://github.com/VeranosTech/CyLP.git@py3 ^
+echo
+
 call deactivate
 cmd.exe /K
--- install_pkg.sh
+++ install_pkg.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# set environment
+# environment variables
 export MACOSX_DEPLOYMENT_TARGET=10.10
 export CYLP_USE_CYTHON=TRUE
 export COIN_INSTALL_DIR=~/anaconda3/envs/vopt/
@@ -10,24 +10,40 @@ export GLPK_INC_DIR=~/anaconda3/envs/vopt/include
 export BUILD_GLPK=1
 
 echo "Python package installing..."
-source activate vopt && \
+source activate vopt
+
+echo "Common Anaconda Packages:" && \
 conda install --yes --quiet \
-anaconda alembic beautifulsoup4 constantly coverage cvxcanon cvxopt ecos django flask gevent greenlet hyperlink incremental ipyparallel krb5 \
-markdown pymongo psycopg2 requests scrapy simplejson sphinx sphinx_rtd_theme toolz twisted werkzeug && \
-conda install --yes --quiet -c conda-forge coincbc fabric3 fastparquet feather-format glpk jupyter_nbextensions_configurator multiprocess pyarrow && \
-conda update --yes --quiet libgcc && \
-conda install --yes --quiet gunicorn && \
-conda install --yes --quiet -c conda-forge pudb uwsgi \
+anaconda alembic beautifulsoup4 constantly coverage cvxcanon cvxopt django ecos flask \
+gevent greenlet hyperlink incremental ipyparallel krb5 \
+markdown pymongo psycopg2 requests scrapy simplejson sphinx sphinx_rtd_theme \
+toolz twisted werkzeug \
 && \
+echo "Common Anaconda Packages in Conda-Forge:" && \
+conda install --yes --quiet -c conda-forge \
+awscli fabric3 fastparquet feather-format glpk jupyter_nbextensions_configurator \
+multiprocess pyarrow \
+&& \
+echo "Linux/OSX Anaconda Packages:" &&  \
+conda install --yes --quiet \
+gunicorn \
+&& \
+echo "Linux/OSX Anaconda Packages in Conda-Forge:" && \
+conda install --yes --quiet -c conda-forge \
+coincbc pudb uwsgi \
+&& \
+echo "Common pip Packages:" && \
 pip install \
-autopep8 awscli coreapi cvxpy django-crispy-forms django-filter django-guardian django-jinja djangorestframework \
-eve flask-restplus flask-security flask_sqlalchemy json-rpc nbsphinx SQLAlchemy-Continuum tushare \
+autopep8 awscli coreapi cvxpy django-crispy-forms django-filter django-guardian \
+django-jinja djangorestframework \
+eve flask-restplus flask-security flask_sqlalchemy json-rpc nbsphinx \
+SQLAlchemy-Continuum tushare \
 && \
+echo "Jupyter notebook setting:" && \
 ipcluster nbextension enable --user && \
 jupyter nbextensions_configurator enable --user && \
-echo
 
-echo "CyLP package for Python3 installing..."
+echo "CyLP package for Python3 installing..." && \
 pip install git+https://github.com/VeranosTech/CyLP.git@py3
 
 source deactivate
